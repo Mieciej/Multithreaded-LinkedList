006f64589378128fcd42eaa770f4950c303ad096
Maciej <40804130+Mieciej@users.noreply.github.com>
2023-05-31 20:30:20 +0200

    Changed number of testes

--- src/main.c
+++ src/main.c
@@ -82,16 +82,15 @@ int main(int argc, char const *argv[])
     pthread_mutexattr_settype(&recursiveAttrs, PTHREAD_MUTEX_RECURSIVE);
 
     pthread_mutex_init(&list.mutex, &recursiveAttrs);
-    pthread_t t[5];
+    pthread_t t[4];
 
     pthread_create(&t[0],NULL,test_add,(void*)&list);
     pthread_create(&t[1],NULL,test_iterate,(void*)&list);
     pthread_create(&t[2],NULL,test_remove,(void*)&list);
     pthread_create(&t[3],NULL,test_poll,(void*)&list);
-    pthread_create(&t[4],NULL,test_poll,(void*)&list);
 
 
-    for (size_t i = 0; i < 5; i++)
+    for (size_t i = 0; i < 4; i++)
     {
         pthread_join(t[i], NULL);
     }
